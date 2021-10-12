785da7680727f066f31d61ca4d60f09bc50903d0
Tosba74 <bastm.ua42@gmail.com>
2021-10-12 12:36:19 +0200

    jsais pas ce que jai touche

--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -6,7 +6,7 @@
 /*   By: bmangin <bmangin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 02:00:38 by bmangin           #+#    #+#             */
-/*   Updated: 2021/10/11 17:51:44 by bmangin          ###   ########lyon.fr   */
+/*   Updated: 2021/10/11 20:21:41 by bmangin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,11 +88,16 @@
 
 void	create_token(char *input, int len)
 {
-	int	i;
+	int		i;
+	char	*tmp;
 
-	i = 0;
-	while (++i < len)
-		printf("\033[32m%c\033[0m", input[i]);
+	i = -1;
+	tmp = wrmalloc(sizeof(char) * (len - 1));
+	input++;	
+	while (++i < len - 1)
+		tmp[i] = input[i];
+	tmp[i] = 0;
+	printf("\033[32m|%s|\033[0m", tmp);
 }
 
 void	create_token_envname(char *input, int len)
@@ -116,33 +121,18 @@ void	create_token_envalue(char *input, int len)
 int	add_var_env(t_global *g, char *input)
 {
 	(void)g;
-	int		i;
+	// int		i;
 	char	c;
-	char 	name;
+	// char 	name;
 
-	i = 0;
+	// i = 0;
 	c = input[0];
-	while (ft_iscapital(input[i]))
-	{
-		
-		
-		create_token(input, i);	
-		return (i);
-	}
-	}
+	// while (ft_iscapital(input[i]))
+	// {
+		// return (i);
+	// }
+	// }
 	dprintf(2, "\nAAAAAaaaaahhhHHHH\n");
-	return (0);
-}
-
-void	lexer(t_global *g)
-{
-	char	*input;
-	int		i;
-
-	input = get_last_input(g);
-	i = -1;
-	while (input[++i])
-	dprintf(2, "\nOOOOOooooohhhHHHH\n");
 	return (0);
 }
 
@@ -177,11 +167,15 @@ void	lexer(t_global *g)
 	while (input[++i])
 	{
 		if (is_spec_char(input[i]) == 0)
-			i += add_var_env(g, input + i);
-		else if (is_spec_char(input[i]) == 1 || is_spec_char(input[i]) == 2)
-			i += token_cote(g, input + i);
-		else
 			ft_putchar(input[i]);
+		else
+		{
+			if (is_spec_char(input[i]) == 0)
+				i += add_var_env(g, input + i);
+			else if (is_spec_char(input[i]) == 1 || is_spec_char(input[i]) == 2)
+				i += token_cote(g, input + i);
+			ft_putchar('\n');
+		}
 	}
 }
 
