056580243187e2c9218013fa4b214a7ca60d426f
MntiKor <astucky@student.42lyon.fr>
2021-11-23 20:59:20 +0100

    started redir errors interface

--- srcs/parsing/redir_exp.c
+++ srcs/parsing/redir_exp.c
@@ -6,28 +6,31 @@
 /*   By: bmangin <bmangin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 16:10:42 by bmangin           #+#    #+#             */
-/*   Updated: 2021/11/23 15:26:08 by astucky          ###   ########lyon.fr   */
+/*   Updated: 2021/11/23 20:53:24 by astucky          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minish.h"
 
-/* PETIT RECAP DES MODIFS
+static void	remove_redir_tok(t_token **tok)
+{
+	t_token	*next;
 
-gestion d'erreurs:
-access(path, F_OK) renvoie 0 si le fichier existe ou -1 s'il n'existe pas
-du coup tu peux poser l'erreur "no such file or directory" en te servant de la valeur de create
+	next = (*tok)->next;
+	remove_cell_tok(*tok);
+	*tok = next;
+	while (*tok && (*tok)->type == SPC)
+	{
+		next = (*tok)->next;
+		remove_cell_tok(*tok);
+		*tok = next;
+	}
+}
 
-boucle inf:
-maintenant remove_redir_tok prend en parametre un t_token **
-sinon tok ne passe pas a l'addresse suivante dans skip_redir, et du coup on se retrouve avec un pointeur vide, ce qui occasionnait un -1...
-
-*/
-
-static int	try_open(t_token *token, char *path)
+static int	try_open(t_token *tok, char *path)
 {
 	struct stat	buf;
-	int			create;
+/*	int			create;
 
 	create = access(path, F_OK);
 	buf.st_mode = 0;
@@ -51,14 +54,42 @@ static int	try_open(t_token *token, char *path)
 			|| (((!(buf.st_mode & S_IRUSR) && token->type == REDIR_L)
 			|| (!(buf.st_mode & S_IWUSR) && (token->type == REDIR_R
 					|| token->type == REDIR_RD))) && !create))
-			ft_err(token->value, 7);
+			ft_err(token->value, 7); */
+
+	buf.st_mode = 0;
+	stat(tok->value, &buf);
+	if ((buf.st_mode & S_IFDIR))
+	{
+		if (tok->type == REDIR_L)
+		{
+			ft_err("stdin", 8);
+			remove_redir_tok(&tok);
+			return (-1);
+		}
+		else if (tok->type == REDIR_R || tok->type == REDIR_RD)
+		{
+			ft_err(tok->value, 8);
+			remove_redir_tok(&tok);
+			return (-1);
+		}
+	}
+	else if ((bool)(buf.st_mode & S_IFMT))
+	{
+		if ((!(((bool)buf.st_mode & S_IRUSR)) && tok->type == REDIR_L)
+			|| ((!((bool)(buf.st_mode & S_IWUSR)) && (tok->type == REDIR_R
+					|| tok->type == REDIR_RD))))
+		{
+			ft_err(tok->value, 7);
+			remove_redir_tok(&tok);
+			return (-1);
+		}
 		else
 		{
-			if (token->type == REDIR_R)
+			if (tok->type == REDIR_R)
 				return (open(path, O_CREAT | O_RDWR | O_TRUNC, 0644));
-			if (token->type == REDIR_RD)
+			if (tok->type == REDIR_RD)
 				return (open(path, O_CREAT | O_RDWR | O_APPEND, 0644));
-			if (token->type == REDIR_L)
+			if (tok->type == REDIR_L)
 				return (open(path, O_RDONLY));
 		}
 	}
@@ -82,21 +113,6 @@ static void	replace_fd(int *old, int new, int default_fd, bool *change)
 		close(*old);
 	*old = new;
 	*change = true;
-}
-
-static void	remove_redir_tok(t_token **tok)
-{
-	t_token	*next;
-
-	next = (*tok)->next;
-	remove_cell_tok(*tok);
-	*tok = next;
-	while (*tok && (*tok)->type == SPC)
-	{
-		next = (*tok)->next;
-		remove_cell_tok(*tok);
-		*tok = next;
-	}
 }
 
 void	close_all_fd(t_pipe *pipe)
@@ -138,3 +154,60 @@ int	skip_redir(t_pipe *pipe, t_token *tok)
 	}
 	return (1);
 }
+/*
+stat(tok->value, &buf);
+	if ((buf.st_mode & S_IFDIR))
+	{
+		if (tok->type == REDIR_L)
+		{
+			ft_err("stdin", 8);
+			remove_redir_tok(tok);
+			return (-1);
+		}
+		else if (tok->type == REDIR_R || tok->type == REDIR_RD)
+		{
+			ft_err(tok->value, 8);
+			remove_redir_tok(tok);
+			return (-1);
+		}
+	}
+	else if ((bool)(buf.st_mode & S_IFMT))
+	{
+		if ((!(((bool)buf.st_mode & S_IRUSR)) && tok->type == REDIR_L)
+			|| ((!((bool)(buf.st_mode & S_IWUSR)) && (tok->type == REDIR_R
+					|| tok->type == REDIR_RD))))
+		{
+			ft_err(tok->value, 7);
+			remove_redir_tok(tok);
+			return (-1);
+		}
+		else
+		{
+			if (tok->type == REDIR_R)
+			{
+				new->fd_out = open(tok->value, O_CREAT | O_RDWR | O_TRUNC, 0644);
+				new->out = true;
+				remove_redir_tok(tok);
+				return (0);
+			}
+			else if (tok->type == REDIR_RD)
+			{
+				new->fd_out = open(tok->value, O_CREAT | O_RDWR | O_TRUNC, 0644);
+				new->out = true;
+				remove_redir_tok(tok);
+				return (0);
+			}
+			else if (tok->type == REDIR_L)
+			{
+				new->fd_out = open(tok->value, O_RDONLY);
+				new->out = true;
+				remove_redir_tok(tok);
+				return (0);
+			}
+			if (tok->type == REDIR_RD || tok->type == REDIR_R)
+				replace_fd(&pipe->fd_out, new_fd, 1, &pipe->out);
+			else
+				replace_fd(&pipe->fd_in, new_fd, 0, &pipe->in);
+			remove_redir_tok(&tok);
+		}
+*/
